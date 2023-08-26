799790897834af62e19b0617d76ed456846bffd2
Pritam Bose <pritambose040@gmail.com>
2023-08-27 00:50:46 +0530

    Aug  27

---
+++ GFG Potd/Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "naba";
+        isPalindrome(str);
+    }
+
+    private static void isPalindrome(String str) {
+        int i = 0;
+        int j = str.length()-1;
+        boolean flag = true;
+        while(i<=j){
+            if(str.charAt(i++) != str.charAt(j--)){
+                flag = false;
+            }
+        }
+
+        if(!flag){
+            System.out.println("Not Palindrome");
+        }
+        else{
+            System.out.println("Palindrome");
+        }
+    }
+}
--- Gfg Potd/ReverseString.java
+++ Gfg Potd/ReverseString.java
@@ -1,24 +1,14 @@
 public class ReverseString {
     public static void main(String[] args) {
-        String str = "naba";
-        isPalindrome(str);
+        String str = "Pritam";
+        System.out.println(reverse(str));
     }
 
-    private static void isPalindrome(String str) {
-        int i = 0;
-        int j = str.length()-1;
-        boolean flag = true;
-        while(i<=j){
-            if(str.charAt(i++) != str.charAt(j--)){
-                flag = false;
-            }
+    private static String reverse(String str) {
+        String ans = "";
+        for(int i=str.length()-1;i>=0;i--){
+            ans += str.charAt(i);
         }
-
-        if(!flag){
-            System.out.println("Not Palindrome");
-        }
-        else{
-            System.out.println("Palindrome");
-        }
+        return ans;
     }
 }
